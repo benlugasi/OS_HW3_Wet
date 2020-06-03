82f30856a6c2aa04d3d7ea959756c184c2390e11
Ben Lugasi <benlugasi@campus.technion.ac.il>
2020-06-03 16:24:39 +0000

    Wait as is in dry

--- Barrier.cpp
+++ Barrier.cpp
@@ -5,9 +5,22 @@ Barrier::Barrier(unsigned int num_of_threads) {
 }
 
 void Barrier::wait() {
+    mutex.wait();
+    count +=1;
+    if(count == n) barrier.signal();
+    mutex.signal();
 
+    barrier.wait();
+    barrier.signal();
+
+    //critical point
+
+    mutex.wait();
+    count -=1;
+    if(count == 0) barrier.wait();
+    mutex.signal();
 }
 
 Barrier::~Barrier() {
-//
+
 }
